18d164a791ec2a62244ebfde63ba49c15e03aeba
VJKUMAR <115359380+letsmailvjkumar@users.noreply.github.com>
2023-06-22 15:55:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,3 +2,22 @@ window.promises = [];
 
 // Do not change the code above this
 // add your promises to the array `promises`
+for (let i = 0; i < 5; i++) {
+  const delay = Math.floor(Math.random() * 5) + 1; // Generate random delay between 1 and 5 seconds
+
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(`Promise ${i + 1} resolved after ${delay} seconds`);
+    }, delay * 1000);
+  });
+
+  promises.push(promise);
+}
+Promise.any(promises)
+  .then((result) => {
+    const outputElement = document.getElementById("output");
+    outputElement.innerText = result;
+  })
+  .catch((error) => {
+    console.error(error);
+  });
